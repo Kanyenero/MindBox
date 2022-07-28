f954ba760ca9eba2d4833bd328aa52ad51603911
Zakhar Khakhalin <khakhalinz@yandex.ru>
2022-07-28 22:27:30 +0300

    Added figures unit tests

---
+++ MindBox.Task1/Geometry.Tests/CircleTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using MindBox.Task1.Geometry;
+
+namespace MindBox.Task1.Geometry.Tests
+{
+    [TestFixture]
+    public class CircleTests
+    {
+        [TestCase(0)]
+        [TestCase(-1)]
+        public void TestCreate_RadiusLessOrEqualsZero_ThrowsException(double radius)
+        {
+            Assert.Throws<ArgumentException>(delegate { Circle.Create(radius); });
+        }
+
+        [TestCase(10, ExpectedResult = Math.PI * 100)]
+        public double TestGetSquare_ReturnedValue_Correct(double radius)
+        {
+            return Circle.Create(radius).GetSquare();
+        }
+    }
+}
---
+++ MindBox.Task1/Geometry.Tests/TriangleTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+
+namespace MindBox.Task1.Geometry.Tests
+{
+    [TestFixture]
+    public class TriangleTests
+    {
+        [TestCase(0, 1, 1)]
+        [TestCase(1, 0, 1)]
+        [TestCase(1, 1, 0)]
+        public void TestCreate_SideEqualsZero_ThrowsException(double firstSideLength, double secondSideLength, double thirdSideLength)
+        {
+            Assert.Throws<ArgumentException>(delegate { Triangle.Create(firstSideLength, secondSideLength, thirdSideLength); });
+        }
+
+        [TestCase(-1, 1, 1)]
+        [TestCase(1, -1, 1)]
+        [TestCase(1, 1, -1)]
+        public void TestCreate_SideLessThanZero_ThrowsException(double firstSideLength, double secondSideLength, double thirdSideLength)
+        {
+            Assert.Throws<ArgumentException>(delegate { Triangle.Create(firstSideLength, secondSideLength, thirdSideLength); });
+        }
+
+        [TestCase(10, 1, 1)]
+        [TestCase(1, 10, 1)]
+        [TestCase(1, 1, 10)]
+        public void TestCreate_ExistenceCondition_ThrowsException(double firstSideLength, double secondSideLength, double thirdSideLength)
+        {
+            Assert.Throws<ArgumentException>(delegate { Triangle.Create(firstSideLength, secondSideLength, thirdSideLength); });
+        }
+
+        [TestCase(3, 4, 5, ExpectedResult = true)]
+        [TestCase(4, 5, 6, ExpectedResult = false)]
+        public bool TestTriangle_IsRight_HasCorrectValue(double firstSideLength, double secondSideLength, double thirdSideLength)
+        {
+            return Triangle.Create(firstSideLength, secondSideLength, thirdSideLength).IsRight;
+        }
+
+        [TestCase(3, 4, 5, ExpectedResult = 6)]
+        public double TestGetSquare_ReturnedValue_Correct(double firstSideLength, double secondSideLength, double thirdSideLength)
+        {
+            return Triangle.Create(firstSideLength, secondSideLength, thirdSideLength).GetSquare();
+        }
+    }
+}
