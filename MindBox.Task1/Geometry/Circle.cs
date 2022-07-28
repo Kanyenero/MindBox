e847f631fc8cc31139f58cd7b2d0e545ce41802f
Zakhar Khakhalin <khakhalinz@yandex.ru>
2022-07-28 22:26:08 +0300

    Added new figures

---
+++ MindBox.Task1/Geometry/Circle.cs
@@ -0,0 +1,34 @@
+﻿namespace MindBox.Task1.Geometry
+{
+    public class Circle : FlatFigure
+    {
+        public double Radius 
+        { 
+            get; 
+            private set; 
+        }
+
+        public static Circle Create(double radius)
+        {
+            ValidateArguments(radius);
+
+            return new Circle(radius);
+        }
+
+        private static void ValidateArguments(double radius)
+        {
+            if (radius <= 0.0)
+                throw new ArgumentException($"Argument '{nameof(radius)}' cannot be less or equals zero. Value was '{radius}'.");
+        }
+
+        private Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double GetSquare()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+}
